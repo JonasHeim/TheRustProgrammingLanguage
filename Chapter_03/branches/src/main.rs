8f70980bce5cfaab04721a17fcef80bb46e761e3
JonasHeim <heimjonas@yahoo.de>
2019-05-27 12:46:06 +0200

    If in let statement

--- Chapter_03/branches/src/main.rs
+++ Chapter_03/branches/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let number = 6;
-
-    if number % 4 == 0 {
-        println!("number is divisable by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisable by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisable by 2");
+    let condition = true;
+    let number = if condition {
+        5
     } else {
-        println!("number is not divisable by either 4, 3 or 2");
-    }
+        6
+    };
+
+    println!("The value of number is {}", number);
 }
