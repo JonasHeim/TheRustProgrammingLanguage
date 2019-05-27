777203bf2703a994ca3e4d0781d1bd4ad0c5c93e
JonasHeim <heimjonas@yahoo.de>
2019-05-27 16:06:00 +0200

    Function returning tuple

--- Chapter_04/ownership/src/main.rs
+++ Chapter_04/ownership/src/main.rs
@@ -1,22 +1,12 @@
 fn main() {
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
 
-    println!("The value of s1 is: {}", s1);
+    let (s2, len) = calculate_length(s1);
 
-    let s2 = String::from("hello");
-
-    println!("The value of s2 is: {}", s2);
-
-    let s3 = takes_and_gives_ownership_back(s2);
-
-    println!("The value of s3 is: {}", s3);
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("world");
-    some_string
-}
-
-fn takes_and_gives_ownership_back(a_string: String) -> String {
-    a_string
+fn calculate_length(some_string: String) -> (String, usize) {
+    let length = some_string.len();
+    (some_string, length)
 }
