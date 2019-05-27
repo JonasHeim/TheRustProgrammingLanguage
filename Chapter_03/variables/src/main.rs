edfa84ce5125f163f894b570efdfc958b3145c53
JonasHeim <heimjonas@yahoo.de>
2019-05-27 12:09:54 +0200

    Array index access

--- Chapter_03/variables/src/main.rs
+++ Chapter_03/variables/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let a = [1, 2, 3, 4, 5];
 
-    let five_hundred = x.0;
+    let first = a[0];
 
-    let six_point_four = x.1;
-
-    let one = x.2;
+    let second = a[1];
 }
