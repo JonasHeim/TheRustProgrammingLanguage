cf849e5ef0703dcefc29b1b501545cb27b44f00d
JonasHeim <heimjonas@yahoo.de>
2019-05-28 16:10:17 +0200

    Iterate through vector

--- Chapter_08/vectors/src/main.rs
+++ Chapter_08/vectors/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
-    let v = vec![1, 2, 3, 45];
+    let mut v = vec![100, 32, 57];
 
-    let third: &i32 = &v[2];
-    println!("third {}", third);
-    let third: Option<&i32> = v.get(2);
+    for i in &v {
+        println!("{}", i);
+    }
+    
+    for i in &mut v {
+        *i += 50;
+    }
+
+    for i in &v {
+        println!("{}", i);
+    }
+    
 }
