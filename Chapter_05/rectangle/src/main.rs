8036d4a788a492afe80ff0340de6529aa0cd966c
JonasHeim <heimjonas@yahoo.de>
2019-05-28 11:33:20 +0200

    Added debug output for the struct

--- Chapter_05/rectangle/src/main.rs
+++ Chapter_05/rectangle/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -8,6 +9,9 @@ fn main() {
         width: 50,
         height: 30,
     };
+
+    println!("rectangle1 is {:?}", rectangle1);
+    println!("rectangle1 is {:#?}", rectangle1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
