992ade4809ed072cd837797ef2ca74b9900eef72
JonasHeim <heimjonas@yahoo.de>
2019-05-28 16:05:17 +0200

    Reading from a vector

--- Chapter_08/vectors/src/main.rs
+++ Chapter_08/vectors/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    let mut v = Vec::new();
+    let v = vec![1, 2, 3, 45];
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+    let third: &i32 = &v[2];
+    println!("third {}", third);
+    let third: Option<&i32> = v.get(2);
 }
