58f591eb15681c6528e5134124c5241b80e504db
JonasHeim <heimjonas@yahoo.de>
2019-05-28 12:19:26 +0200

    Enums with implementation

--- Chapter_06/enums/src/main.rs
+++ Chapter_06/enums/src/main.rs
@@ -1,9 +1,19 @@
-fn main() {
-    enum IpAddr {
-        V4(u8, u8, u8, u8),
-        V6(String),
-    }
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+        println!("{:#?}", &self);
+    }
+}
+
+fn main() {
+    let msg1 = Message::Write(String::from("hello world!"));
+    msg1.call();
 }
