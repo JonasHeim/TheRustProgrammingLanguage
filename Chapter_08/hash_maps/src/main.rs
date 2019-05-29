488b15a7ddf06ffaf10d0810995adc8b02954e85
JonasHeim <heimjonas@yahoo.de>
2019-05-29 11:11:15 +0200

    Get value out of HasMap

--- Chapter_08/hash_maps/src/main.rs
+++ Chapter_08/hash_maps/src/main.rs
@@ -1,12 +1,17 @@
 use std::collections::HashMap;
 
 fn main() {
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = HashMap::new();
 
-    let mut map = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 
-    map.insert(field_name, field_value);
+    let team_name = String::from("Blue");
 
-    println!("{} - {}", field_name, field_value);
+    let score = scores.get(&team_name);
+
+    match score {
+        None => (),
+        Some(i) => println!("Score of team {} is {}", team_name, i),
+    }
 }
