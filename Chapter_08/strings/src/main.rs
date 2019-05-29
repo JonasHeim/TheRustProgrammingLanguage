2044a8bae532196d33b74a68999cd988bfd4b550
JonasHeim <heimjonas@yahoo.de>
2019-05-29 10:43:23 +0200

    String push_str and push

--- Chapter_08/strings/src/main.rs
+++ Chapter_08/strings/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut s = String::from("foo");
-    s.push_str("bar");
+    let mut s1 = String::from("foo");
 
-    println!("{}", s);
+    let s2 = "bar";
+
+    s1.push_str(s2);
+
+    println!("s2 is {}", s2);
+
+    s1.push('-');
+
+    println!("s1 is {}", s1);
 }
