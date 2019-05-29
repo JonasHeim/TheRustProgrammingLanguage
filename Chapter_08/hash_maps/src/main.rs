2e74c701b8cdab15ebb2bcdd37ec469ab5df7068
JonasHeim <heimjonas@yahoo.de>
2019-05-29 11:15:00 +0200

    Overwriting a value of a HashMap

--- Chapter_08/hash_maps/src/main.rs
+++ Chapter_08/hash_maps/src/main.rs
@@ -4,18 +4,8 @@ fn main() {
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    scores.insert(String::from("Blue"), 50);
 
-    let team_name = String::from("Blue");
+    println!("{:#?}", scores);
 
-    let score = scores.get(&team_name);
-
-    match score {
-        None => (),
-        Some(i) => println!("Score of team {} is {}", team_name, i),
-    }
-    
-    for (key, value) in &scores {
-        println!("Team {} has {} points.", key, value);
-    }
 }
