4581a30a7e108fbab6e1adc6bca51a30d9c818a1
JonasHeim <heimjonas@yahoo.de>
2019-05-29 11:47:14 +0200

    Distinguishing error kinds

--- Chapter_09/recoverable_errors/src/main.rs
+++ Chapter_09/recoverable_errors/src/main.rs
@@ -1,12 +1,21 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(error) => {
+                    panic!("There was a problem creating the file: {:?}", error);
+                }
+            }
+        },
         Err(error) => {
             panic!("There was a problem opening the file: {:?}", error);
-        }
+        },
     };
 }
