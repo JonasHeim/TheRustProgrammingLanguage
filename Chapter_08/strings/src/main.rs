4688a0f7816211a54cf5438310ce56cc2f9c8231
JonasHeim <heimjonas@yahoo.de>
2019-05-29 10:46:02 +0200

    String + operator

--- Chapter_08/strings/src/main.rs
+++ Chapter_08/strings/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let mut s1 = String::from("foo");
+    let s1 = String::from("Hello ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
 
-    let s2 = "bar";
-
-    s1.push_str(s2);
-
-    println!("s2 is {}", s2);
-
-    s1.push('-');
-
-    println!("s1 is {}", s1);
+    println!("{}", s3);
+    println!("{}", s2);
+    // println!("{}", s1);
+    // s1 not valid anymore, was moved at + operator
 }
